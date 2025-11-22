57006a476ea593026ec2adc1bf3fca149ca95990
virlpp <138609022+virlpp@users.noreply.github.com>
2025-11-21 22:05:44 -0300

    Create Vaca.java

---
+++ TP7/Ejercicio4/Vaca.java
@@ -0,0 +1,10 @@
+class Vaca extends Animal {
+    public Vaca(String nombre) {
+        super(nombre);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println(" y hago Muuuuu");
+    }
+}
