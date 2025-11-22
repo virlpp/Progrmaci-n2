4fbd2385f373916a4241237164dfbb5f4b08646b
virlpp <138609022+virlpp@users.noreply.github.com>
2025-11-21 21:45:13 -0300

    Create Empleado.java

---
+++ TP7/Ejercicio3/Empleado.java
@@ -0,0 +1,14 @@
+abstract class Empleado {
+    
+    protected String nombre;
+    
+    public Empleado (String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public abstract double calcularSueldo();
+}
